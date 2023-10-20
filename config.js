6555e959a00428049cf4c8bbe5b76e0d743be185
Trungaladin <99710110+Trungaladin@users.noreply.github.com>
2023-10-20 19:25:34 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,11 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100012788884529';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng vợ yêu';
+const contentCard = 'Chúc bbi của anh 20/10 vui vẻ. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+// phần dưới để code, nếu muốn chỉnh ảnh đơn giản với base64
+
+const giftImage = 'bbi.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
